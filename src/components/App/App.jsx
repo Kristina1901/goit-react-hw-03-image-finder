c45543e675bbcdc4b45eea57278257913503649c
Kristina Tuzova <ktuzova1901@gmail.com>
2022-05-20 05:14:50 +0300

    page

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -112,7 +112,7 @@ export default class App extends Component {
   };
   componentDidUpdate(prevProps, prevState) {
     if (prevState.imageName !== this.state.imageName) {
-      this.setState({ image: [], status: 'pending', value: false });
+      this.setState({ image: [], status: 'pending', value: false, page: 1 });
       this.fetchData(this.state.imageName, this.state.page);
     }
     if (prevState.page !== this.state.page) {
